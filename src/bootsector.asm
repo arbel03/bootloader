b094d571b2dfbc73611f7a26820d0b7933f94ff4
Arbel Israeli <arbel03@gmail.com>
2017-11-30 18:15:14 +0200

    Added print functions Printing disk number for debugging

--- src/bootsector.asm
+++ src/bootsector.asm
@@ -5,17 +5,22 @@ start:
     ; Set stack, will be located at (0:0x7c00+0x7c00)
     mov sp, 0x7c00
     
-    mov bx, hello
+    mov bx, disk_number_message
     call print_string
+
+    mov ax, dx
+    xor ah, ah
+    call print_register
 
     jmp $
 
 %include "src/print.asm"
 
-hello db 'The quick brown fox jumps over the lazy dog!',0
+hex db '0123456789abcdef'
+disk_number_message db 'Disk number: ',0
 
 times 510-($-$$) db 0
 dw 0xaa55
 
-times 512 db 'x','a'
+times 512 db 'xa'
 
