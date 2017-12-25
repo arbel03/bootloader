2afef22dfaa05e5db37f5681e271764d81dcfe67
Arbel Israeli <arbel03@gmail.com>
2017-12-25 15:49:52 +0200

    Passing first adress of free memory to kernel

--- Makefile
+++ Makefile
@@ -1,3 +1,2 @@
 $(iso): $(kernel)
-	@nasm -f bin -o $(iso) -i bootloader/ bootloader/src/bootloader.asm 
-	
+	@nasm -f bin -o $(iso) -i bootloader/ bootloader/src/bootloader.asm
--- src/stage2/elf.asm
+++ src/stage2/elf.asm
@@ -5,18 +5,21 @@ load_elf:
     mov ebx, kernel_start
     mov esi, [kernel_start+0x1C] ; program header offset
     mov ebx, esi
-
+    
     mov ecx, [kernel_start+0x10+ebx] ; p_filesz- Size in bytes of the segment in the file image.
+    mov edx, [kernel_start+0x14+ebx] ; p_memsz- Size in bytes of the segment in memory
     mov edi, [kernel_start+0x08+ebx] ; p_vaddr- Virtual address of the segment in memory.
+    add edx, edi
+    mov [first_free_address], edx ; Store first free adress
     mov esi, [kernel_start+0x04+ebx] ; p_offset- Offset of the segment in the file image.
     add esi, kernel_start
 
     rep movsb
 
-    mov dword [0xb8000], 0x2f4b2f4f
-
     mov eax, [kernel_start+0x18] ; Entry point
-    jmp eax
+    mov edi, [first_free_address]
+    call eax
 
     ret
 
+first_free_address: dw 0
